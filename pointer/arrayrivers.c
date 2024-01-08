2eda871c1a2b2ec037252ffb05d8b5c0c9a0e939
RABARI NARESH <148762716+Nikks27@users.noreply.github.com>
2024-01-08 13:21:29 +0530

    Add files via upload

---
+++ pointer/arrayrivers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{
+	int n;
+	printf("enter the array size : ");
+	scanf("%d",&n);
+	
+	int a[n];
+	int i;
+    
+	for(i=0; i<n; i++)
+	{
+		printf("enter the value a[%d]",i);
+		scanf("%d",&a[i]);
+	}
+	int *pointer= &a[n-1];	
+	for(i=n-1;i>=0; i--);
+	{
+		printf("%d\n",*pointer);
+		pointer-=1;
+	}
+}
